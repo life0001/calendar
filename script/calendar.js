ed6c428ed90119a5224cdab1a2a146c947cd4899
peter <life0001@126.com>
2016-02-22 10:39:34 +0800

    Update calendar.js

--- script/calendar.js
+++ script/calendar.js
@@ -11,15 +11,16 @@ YUI({
         }
     }
 }).use('trip-calendar', function(Y) {
-    //配置信息
+    //配置日历
     var oCal = new Y.TripCalendar({
         minDate         : new Date,  //设置最小时间（过去日期不可选）
         triggerNode     : '#J_DepDate, #J_DepDate3', //行程开始日期
         finalTriggerNode:'#J_DepDate2, #J_DepDate4', //行程结束日期
         count            : 1  //日历个数
     });
+    //日历事件
     oCal.on('dateclick', function(e) {
-        var athis=this.getCurrentNode()._node; //点击获取获得当前的元素
+        var athis=this.getCurrentNode()._node; //点击获取当前的元素
         $(athis).find('input').val(e.date);     //e.date是选择日期
     });
     $('.startDate').click(function(e){
